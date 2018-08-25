a8f09fa844fdc4565050d19ed1b3673fe0962392
Dominic Meiser <dmeiser79@gmail.com>
2018-08-25 10:12:26 -0600

    Add local emacs variables to configure project build directories

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
-((c-mode . ((helm-make-build-dir . "build-debug")))
- (c++-mode . ((helm-make-build-dir . "build-debug"))))
+((c-mode . ((helm-make-build-dir . "build-debug")
+	    (projectile-project-compilation-dir . "build-debug")))
+ (c++-mode . ((helm-make-build-dir . "build-debug")
+	      (projectile-project-compilation-dir . "build-debug"))))
