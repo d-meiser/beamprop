64ec0d678bcc0274e3bc436e23868aafb8d40800
Dominic Meiser <dmeiser79@gmail.com>
2018-08-28 20:08:20 -0600

    Add implementation of 1D FFT API

--- fft/fft.c
+++ fft/fft.c
@@ -162,7 +162,7 @@ void Field2DPropagate(struct Field2D *field, double k_0, double dz)
 
 Amplitude Field2DGaussian(double x, double y, void *ctx)
 {
-	struct GaussianCtx *gctx = ctx;
+	struct Gaussian2DCtx *gctx = ctx;
 	x -= gctx->mu_x;
 	y -= gctx->mu_y;
 	return 1.0 / (2.0 * M_PI * gctx->sigma_x * gctx->sigma_y) * exp(
@@ -254,5 +254,183 @@ FFT_EXPORT void Field2DAmplitudeGrating(struct Field2D *field, double pitch, dou
 		for (int j = 0; j < field->n; ++j) {
 			row[j] *= mask;
 		}
+	}
+}
+
+void Field1DCreate(struct Field1D *field, int m, double limits[static 2])
+{
+	field->m = m;
+	field->amplitude = aligned_alloc(MIN_ALIGNMENT, m * sizeof(Amplitude));
+	field->fourier_amplitude =
+		aligned_alloc(MIN_ALIGNMENT, m * sizeof(Amplitude));
+	memcpy(field->limits, limits, 2 * sizeof(*limits));
+
+	int dims[1] = {field->m};
+	int inembed[1] = {field->m};
+	int onembed[1] = {field->m};
+	int istride = 1;
+	int ostride = 1;
+	int idist = 0;
+	int odist = 0;
+	field->plan_forward = fftw_plan_many_dft(
+		1, dims, 1,
+		field->amplitude, inembed, istride, idist,
+		field->fourier_amplitude, onembed, ostride, odist,
+		FFTW_FORWARD, FFTW_ESTIMATE);
+	field->plan_backward = fftw_plan_many_dft(
+		1, dims, 1,
+		field->fourier_amplitude, onembed, ostride, odist,
+		field->amplitude, inembed, istride, idist,
+		FFTW_BACKWARD, FFTW_ESTIMATE);
+}
+
+struct Field1D Field1DCopy(struct Field1D *field)
+{
+	struct Field1D copy;
+	Field1DCreate(&copy, field->m, field->limits);
+
+	assert(copy.m == field->m);
+
+	memcpy(copy.amplitude, field->amplitude,
+		field->m * sizeof(*field->amplitude));
+	memcpy(copy.fourier_amplitude, field->fourier_amplitude,
+		field->m * sizeof(*field->fourier_amplitude));
+
+	return copy;
+}
+
+void Field1DFill(struct Field1D *field, AnalyticalField1D f, void *ctx)
+{
+	asm("# Start of Field1DFill\n");
+	double xmin = field->limits[0];
+	double dx = compute_dx(field->limits[0], field->limits[1], field->m);
+	Amplitude *row = __builtin_assume_aligned(field->amplitude, MIN_ALIGNMENT);
+	for (int i = 0; i < field->m; ++i) {
+		double x =  xmin + i * dx;
+		row[i] = f(x, ctx);
+	}
+	asm("# End of Field1DFill\n");
+}
+
+void Field1DDestroy(struct Field1D *field)
+{
+	free(field->amplitude);
+	free(field->fourier_amplitude);
+	fftw_destroy_plan(field->plan_forward);
+	fftw_destroy_plan(field->plan_backward);
+}
+
+void Field1DTransform(struct Field1D *field, int sign)
+{
+	if (sign == FFTW_FORWARD) {
+		fftw_execute(field->plan_forward);
+	} else if (sign == FFTW_BACKWARD) {
+		fftw_execute(field->plan_backward);
+	} else {
+		assert(0);
+	}
+}
+
+void Field1DFillConstant(struct Field1D *field, Amplitude a)
+{
+	Amplitude *row = __builtin_assume_aligned(field->amplitude, MIN_ALIGNMENT);
+	for (int i = 0; i < field->m; ++i) {
+		row[i] = a;
+	}
+}
+
+
+void Field1DPropagate(struct Field1D *field, double k_0, double dz)
+{
+	Field1DTransform(field, FFTW_FORWARD);
+
+	const double lx = field->limits[1] - field->limits[0];
+	Amplitude *row = __builtin_assume_aligned(
+		field->fourier_amplitude, MIN_ALIGNMENT);
+	for (int i = 0; i < field->m; ++i) {
+		const double kx = ki(i, field->m, lx);
+		double k_par = kx * kx / (2.0 * k_0);
+		row[i] *= cexp(I * k_par * dz) / field->m;
+	}
+
+	Field1DTransform(field, FFTW_BACKWARD);
+}
+
+Amplitude Field1DGaussian(double x, void *ctx)
+{
+	struct Gaussian1DCtx *gctx = ctx;
+	x -= gctx->mu_x;
+	return 1.0 / sqrt(2.0 * M_PI * gctx->sigma_x) * exp(
+		-0.5 * x * x / (gctx->sigma_x * gctx->sigma_x));
+}
+
+void Field1DWriteIntensities(struct Field1D *field, FILE *f)
+{
+	double dx = compute_dx(field->limits[0], field->limits[1], field->m);
+	Amplitude *row = __builtin_assume_aligned(
+		field->amplitude, MIN_ALIGNMENT);
+	for (int i = 0; i < field->m; ++i) {
+		double x = field->limits[0] + i * dx;
+		fprintf(f, "%lf %lf %lf  ", x, creal(row[i]), cimag(row[i]));
+	}
+}
+
+void Field1DWriteIntensitiesToFile(struct Field1D *field,
+				   const char *filename)
+{
+	FILE *f = fopen(filename, "w");
+	Field1DWriteIntensities(field, f);
+	fclose(f);
+}
+
+void Field1DSphericalAperture(struct Field1D *field, double radius)
+{
+	double dx = compute_dx(field->limits[0], field->limits[1], field->m);
+	double radius_squared = radius * radius;
+	Amplitude *row = __builtin_assume_aligned(
+		field->amplitude, MIN_ALIGNMENT);
+	for (int i = 0; i < field->m; ++i) {
+		double x = field->limits[0] + i * dx;
+		if (x * x > radius_squared) {
+			row[i] = 0.0;
+		}
+	}
+}
+
+void Field1DThinLens(struct Field1D *field, double k0, double f)
+{
+	double dx = compute_dx(field->limits[0], field->limits[1], field->m);
+	Amplitude *row = __builtin_assume_aligned(
+		field->amplitude, MIN_ALIGNMENT);
+	for (int i = 0; i < field->m; ++i) {
+		double x = field->limits[0] + i * dx;
+		double phase = -0.5 * k0 * x*x / f;
+		double c = cos(phase);
+		double s = sin(phase);
+		row[i] *= c + I * s;
+	}
+}
+
+FFT_EXPORT void Field1DAmplitudeGrating(struct Field1D *field, double pitch, double w)
+{
+	double dx = compute_dx(field->limits[0], field->limits[1], field->m);
+	Amplitude *row = __builtin_assume_aligned(
+		field->amplitude, MIN_ALIGNMENT);
+	for (int i = 0; i < field->m; ++i) {
+		double x = field->limits[0] + i * dx;
+		x = x - floor(x / pitch) * pitch;
+		assert(x >= 0);
+		assert(x <= pitch);
+		double mask = 0.0;
+		if (x < 0.5 * dx) {
+			mask = 0.5 + x / dx;
+		} else if (x >= 0.5 * dx && x < w - 0.5 * dx) {
+			mask = 1.0;
+		} else if (x >= w - 0.5 * dx && x < w + 0.5 * dx) {
+			mask = 0.5 - (x - w) / dx;
+		} else if (x >= pitch - 0.5 * dx) {
+			mask = 0.5 + (x - pitch) / dx;
+		}
+		row[i] *= mask;
 	}
 }
--- fft/fft.h
+++ fft/fft.h
@@ -8,6 +8,18 @@
 
 
 typedef double complex Amplitude;
+
+/** Compute i-th frequency for uniform sampling
+
+    @param i Index of wave number. Assumed to be in [0, n)
+    @param n Number of samples
+    @param l Length of sampling interval
+    @return The i-th frequency
+*/
+FFT_EXPORT double ki(int i, int n, double l);
+
+
+/* 2D API */
 /** Scalar field on uniform cartesian grid */
 struct Field2D {
 	/** 2D array of field amplitudes */
@@ -35,7 +47,7 @@ FFT_EXPORT void Field2DFill(struct Field2D *field, AnalyticalField2D f, void *ct
 FFT_EXPORT void Field2DTransform(struct Field2D *field, int sign);
 FFT_EXPORT void Field2DPropagate(struct Field2D *field, double k_0, double dz);
 
-struct GaussianCtx {
+struct Gaussian2DCtx {
 	double mu_x;
 	double sigma_x;
 	double mu_y;
@@ -45,16 +57,42 @@ FFT_EXPORT Amplitude Field2DGaussian(double x, double y, void *ctx);
 FFT_EXPORT void Field2DFillConstant(struct Field2D *field, Amplitude a);
 FFT_EXPORT void Field2DWriteIntensities(struct Field2D *field, FILE *f);
 FFT_EXPORT void Field2DWriteIntensitiesToFile(struct Field2D *field, const char *filename);
-/** Compute i-th frequency for uniform sampling
-
-    @param i Index of wave number. Assumed to be in [0, n)
-    @param n Number of samples
-    @param l Length of sampling interval
-    @return The i-th frequency
-*/
-FFT_EXPORT double ki(int i, int n, double l);
 FFT_EXPORT void Field2DSphericalAperture(struct Field2D *field, double radius);
 FFT_EXPORT void Field2DThinLens(struct Field2D *field, double k0, double f);
 FFT_EXPORT void Field2DAmplitudeGrating(struct Field2D *field, double pitch, double w);
 
+/* 1D API */
+/** Scalar field on uniform cartesian grid */
+struct Field1D {
+	/** 1D array of field amplitudes */
+	Amplitude *amplitude;
+	/** 1D array of fourier transformed field amplitudes */
+	Amplitude *fourier_amplitude;
+	/** size along x */
+	int m;
+	double limits[2];
+	/** FFT plan for this field */
+	fftw_plan plan_forward;
+	fftw_plan plan_backward;
+};
+
+FFT_EXPORT void Field1DCreate(struct Field1D *field, int m, double limits[static 2]);
+FFT_EXPORT void Field1DDestroy(struct Field1D *field);
+FFT_EXPORT struct Field1D Field1DCopy(struct Field1D *field);
+typedef Amplitude(*AnalyticalField1D)(double x, void *ctx);
+FFT_EXPORT void Field1DFill(struct Field1D *field, AnalyticalField1D f, void *ctx);
+FFT_EXPORT void Field1DTransform(struct Field1D *field, int sign);
+FFT_EXPORT void Field1DPropagate(struct Field1D *field, double k_0, double dz);
+
+struct Gaussian1DCtx {
+	double mu_x;
+	double sigma_x;
+};
+FFT_EXPORT Amplitude Field1DGaussian(double x, void *ctx);
+FFT_EXPORT void Field1DFillConstant(struct Field1D *field, Amplitude a);
+FFT_EXPORT void Field1DWriteIntensities(struct Field1D *field, FILE *f);
+FFT_EXPORT void Field1DWriteIntensitiesToFile(struct Field1D *field, const char *filename);
+FFT_EXPORT void Field1DSphericalAperture(struct Field1D *field, double radius);
+FFT_EXPORT void Field1DThinLens(struct Field1D *field, double k0, double f);
+FFT_EXPORT void Field1DAmplitudeGrating(struct Field1D *field, double pitch, double w);
 #endif
